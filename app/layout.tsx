4060be37e6f35f29d345ce78832fbebd3450cab4
Sarmad Ahmad <sarmadsmac@Sarmads-MacBook-Air.local>
2023-02-17 17:07:13 +0100

    fix background

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body className="bg-[#181818] text-[#FFFFFF] flex flex-col items-center pt-7">
         <main className="w-3/5">
-          <Particle />
           <Navigation />
           {children}
           <Footer />
         </main>
+        <Particle />
       </body>
     </html>
   );
