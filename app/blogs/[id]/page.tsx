afb6aff784a2dce3ace56040b588910b87eecea6
Sarmad Ahmad <sarmadsmac@Sarmads-MacBook-Air.local>
2023-02-18 09:37:59 +0100

    add animation to single blog post

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import { examplePosts } from "../../db/ExampleBlogData";
@@ -5,7 +8,20 @@ import { examplePosts } from "../../db/ExampleBlogData";
 function BlogPost({ params: { id } }: any) {
   const blogPost = examplePosts.find((examplePost) => examplePost.id === parseInt(id));
   return (
-    <div className="font-roboto">
+    <motion.div
+      initial={{ opacity: 0, x: 20 }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        transition: {
+          type: "spring",
+          stiffness: 150,
+          delay: 0.3,
+          duration: 3,
+        },
+      }}
+      className="font-roboto"
+    >
       <Image src={blogPost.image} alt="Blog Post Image" className="h-[500px] rounded-t-lg" />
       <div className="bg-[#222222] p-9 rounded-b-lg">
         <h2 className="text-4xl">{blogPost.title}</h2>
@@ -15,7 +31,7 @@ function BlogPost({ params: { id } }: any) {
         </div>
         <p className="mt-5">{blogPost.text}</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
